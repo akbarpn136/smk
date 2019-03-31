d847c4beae657772a4fc7a4a28e1af8f79c73869
Arizal Akbar Zikri <akbarpn136@gmail.com>
2019-03-31 07:22:32 +0700

    membuat layout baru Auth

---
+++ src/layouts/Auth.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-layout view="hHh Lpr fFf">
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: 'LayoutAuth',
+
+  data () {
+    return {}
+  }
+}
+</script>
