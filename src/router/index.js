aa38464946a04483e8f57df05dc3abf072cb1fc3
Arizal Akbar Zikri <akbarpn136@gmail.com>
2019-03-31 07:23:23 +0700

    import package firebase di index router

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import firebase from 'firebase/app'
+import 'firebase/auth'
 
 import routes from './routes'
 
@@ -20,6 +22,15 @@ export default function (/* { store, ssrContext } */) {
     // quasar.conf.js -> build -> publicPath
     mode: process.env.VUE_ROUTER_MODE,
     base: process.env.VUE_ROUTER_BASE
+  })
+
+  Router.beforeEach((to, from, next) => {
+    if (to.matched.some(item => item.meta.kunci)) {
+      firebase.auth().onAuthStateChanged(user => {
+        if (user) next()
+        else next({name: 'masuk'})
+      })      
+    } else next()
   })
 
   return Router
