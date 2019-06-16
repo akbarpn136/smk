bf281363f07c1e64d0f64bb4964b6b6ab274eac0
Arizal Akbar Zikri <akbarpn136@gmail.com>
2019-06-16 22:23:28 +0700

    tambah fitur firestore

--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -1,15 +1,16 @@
 import firebase from 'firebase/app'
 import 'firebase/auth'
+import 'firebase/firestore'
 
 // leave the export, even if you don't use it
 export default async ({ Vue }) => {
   const config = {
-    apiKey: "AIzaSyCKtfFGnzy1MYWy-TRE5pJ6dIYj5KggheE",
-    authDomain: "smksiswa-c19c7.firebaseapp.com",
-    databaseURL: "https://smksiswa-c19c7.firebaseio.com",
-    projectId: "smksiswa-c19c7",
-    storageBucket: "smksiswa-c19c7.appspot.com",
-    messagingSenderId: "392800305965"
+    apiKey: "AIzaSyDhdq_BvTK2eU9Nx9rvK8DkudjIWZWuf",
+    authDomain: "smksiswa-1e9b2.firebaseapp.com",
+    databaseURL: "https://smksiswa-1e9b2.firebaseio.com",
+    projectId: "smksiswa-1e9b2",
+    storageBucket: "smksiswa-1e9b2.appspot.com",
+    messagingSenderId: "759722842112"
   };
 
   firebase.initializeApp(config)
