27a5b32be94a53ece7440bea69acee26ddd1b0d0
Arizal Akbar Zikri <akbarpn136@gmail.com>
2019-06-23 20:31:14 +0700

    konfigurasi lanjut upload profile picture

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -19,12 +19,44 @@
           flat
           dense
           round
+          @click="showDialogPict = !showDialogPict"
+          aria-label="Profile">
+          <q-avatar size="24px" v-if="user.hasAvatar">
+            <img :src="user.urlAvatar" />
+          </q-avatar>
+
+          <q-icon name="eva-person-outline" v-else />
+        </q-btn>
+
+        <q-btn
+          flat
+          dense
+          round
           @click="cobaKeluar"
           aria-label="Keluar">
           <q-icon name="eva-log-out-outline" />
         </q-btn>
       </q-toolbar>
     </q-header>
+
+    <q-dialog v-model="showDialogPict" persistent>
+      <q-card>
+        <q-bar>
+          <q-icon name="eva-save" />
+            <div>Ganti Profile Picture</div>
+
+            <q-space />
+
+            <q-btn dense flat icon="eva-close-outline" @click="showDialogPict = !showDialogPict">
+              <q-tooltip>Tutup</q-tooltip>
+            </q-btn>
+        </q-bar>
+
+        <q-card-section>
+          <q-uploader :factory="factoryFn" auto-upload dark />
+        </q-card-section>
+      </q-card>
+    </q-dialog>
 
     <q-page-container>
       <router-view />
@@ -36,7 +68,24 @@
 export default {
   name: 'Default',
   data () {
-    return {}
+    return {
+      storageRef: null,
+      showDialogPict: false,
+      user: {
+        id: '',
+        hasAvatar: false,
+        urlAvatar: ''
+      }
+    }
+  },
+  created() {
+    this.$firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.user.id = user.uid
+        this.getAvatar()
+      }
+    })
+    this.storageRef = this.$firebase.storage().ref()
   },
   methods: {
     async cobaKeluar() {
@@ -45,6 +94,39 @@ export default {
         this.$router.push('/masuk')
       } catch (err) {
         console.log(err.message)
+      }
+    },
+    async factoryFn(file) {
+      const pictRef = this.storageRef.child(`smk/${this.user.id}`)
+      const pict = file[0]
+
+      try {
+        await pictRef.put(pict)
+        this.getAvatar()
+
+        return {
+          url: 'data:image/png;base64,',
+          method: 'GET'
+        }
+      }
+      catch (err) {
+        console.log(err.message)
+        this.user.hasAvatar = false
+
+        return {
+          url: '',
+          method: 'GET'
+        }
+      }
+    },
+    async getAvatar() {
+      try {
+        this.user.urlAvatar = await this.storageRef.child(`smk/${this.user.id}`).getDownloadURL()
+
+        if (this.user.urlAvatar) this.user.hasAvatar = true
+      } catch (err) {
+        console.log(err.message)
+        this.user.hasAvatar = false
       }
     }
   }
