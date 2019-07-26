30f7a9d41acaf69c49846f35a30209adb28f00b3
Arizal Akbar Zikri <akbarpn136@gmail.com>
2019-07-26 20:35:59 +0700

    tambah komponen quasar baru

--- quasar.conf.js
+++ quasar.conf.js
@@ -48,7 +48,8 @@ module.exports = function (ctx) {
         'QTooltip',
         'QInput',
         'QUploader',
-        'QAvatar'
+        'QAvatar',
+        'QBadge'
       ],
 
       directives: [
