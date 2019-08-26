c187e3659417017ba2eeef5c1aac271a55919271
Arizal Akbar Zikri <akbarpn136@gmail.com>
2019-08-26 10:52:03 +0700

    update firebase boot

--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -6,12 +6,13 @@ import 'firebase/storage'
 // leave the export, even if you don't use it
 export default async ({ Vue }) => {
   const config = {
-    apiKey: "AIzaSyDhdq_BvTK2eU9Nx9rvK8DkudjIWZWuf",
-    authDomain: "smksiswa-1e9b2.firebaseapp.com",
-    databaseURL: "https://smksiswa-1e9b2.firebaseio.com",
-    projectId: "smksiswa-1e9b2",
-    storageBucket: "smksiswa-1e9b2.appspot.com",
-    messagingSenderId: "759722842112"
+    apiKey: "AIzaSyBJW3gLlgY1GgEohTAosj52Pc5OawFbBoMa",
+    authDomain: "bbta3-bppt.firebaseapp.com",
+    databaseURL: "https://bbta3-bppt.firebaseio.com",
+    projectId: "bbta3-bppt",
+    storageBucket: "bbta3-bppt.appspot.com",
+    messagingSenderId: "246187970753",
+    appId: "1:246187970753:web:5c1911155f6a2388"
   };
 
   firebase.initializeApp(config)
